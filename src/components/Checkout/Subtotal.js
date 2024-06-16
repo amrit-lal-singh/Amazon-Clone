a6f0a62af75347159e67210d8886f5bee07f69ef
Iterate-id <support@iterate.ai.com>
2024-06-16 18:57:44 +0530

    Apply diff to multiple files

--- src/components/Checkout/Subtotal.js
+++ src/components/Checkout/Subtotal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Subtotal.css";
+import mixpanel from 'mixpanel-browser';import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "../../StateProvider";
 import { getBasketTotal } from "../../reducer";
@@ -28,8 +28,7 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
-      <button onClick={(e) => history.push("/payment")}>
-        Proceed to Checkout
+<button onClick={(e) => { mixpanel.track('Proceed to Checkout', { cart_total: getBasketTotal(basket), number_items: basket.length }); history.push("/payment"); }}>        Proceed to Checkout
       </button>
     </div>
   );
