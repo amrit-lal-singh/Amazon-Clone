b88b2b488607f59f25c9c1a8a1f9035d3aa24ac3
amrit-lal-singh <amritlalsingh9920@kgpian.iitkgp.ac.in>
2024-06-17 21:46:39 +0530

    Revert "feat: Adds tracking"

--- src/components/Checkout/Subtotal.js
+++ src/components/Checkout/Subtotal.js
@@ -1,23 +1,13 @@
 import React from "react";
-import mixpanel from 'mixpanel-browser';
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "../../StateProvider";
 import { getBasketTotal } from "../../reducer";
 import { useHistory } from "react-router-dom";
-mixpanel.init('58c886e21f37ceada78f5a7d090d1629', {debug: true, track_pageview: true, persistence: 'localStorage'});
+
 function Subtotal() {
   const history = useHistory();
   const [{ basket }, dispatch] = useStateValue();
-  
-  function handleCheckout() {
-    mixpanel.track('Checkout Initiated', {
-      cart_total: getBasketTotal(basket),
-      number_items: basket.length,
-      add_to_cart_pressed: 'true'
-    });
-    history.push("/payment");
-  }
 
   return (
     <div className="subtotal">
@@ -38,7 +28,7 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
-      <button onClick={handleCheckout}>
+      <button onClick={(e) => history.push("/payment")}>
         Proceed to Checkout
       </button>
     </div>
