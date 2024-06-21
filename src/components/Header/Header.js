552793fecbcd05f17d9a25d692835c4cad6f5a27
Iterate-id <support@iterate.ai.com>
2024-06-21 18:43:38 +0530

    Apply diff to multiple files

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../../StateProvider";
 import { auth } from "../../firebase";
+
+let Itr_user_email = null;
 
 function Header() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -31,7 +33,11 @@ function Header() {
 
       <div className="header-nav">
         <Link to={!user && "/login"}>
-          <div onClick={handleAuthentication} className="header-option">
+          useEffect(() => {
+              Itr_user_email = user?.email || null;
+          }, [user?.email]);
+
+          return ( <div className="header"> <Link to="/"> <img className="header-logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" /> </Link> <div className="header-search"> <input className="header-searchInput" type="text" /> <SearchIcon className="header-searchIcon" /> </div> <div className="header-nav"> <Link to={!user && "/login"}> <div onClick={handleAuthentication} className="header-option"> <span className="header-optionLineOne"> Hello {!user ? "Guest" : user.email} </span> <span className="header-optionLineTwo"> {user ? "Sign Out" : "Sign In"} </span> </div> </Link> <Link to="/orders"> <div className="header-option"> <span className="header-optionLineOne">Returns</span> <span className="header-optionLineTwo">& Orders</span> </div> </Link> <div className="header-option"> <span className="header-optionLineOne">Your</span> <span className="header-optionLineTwo">Prime</span> </div> <Link to="/checkout"> <div className="header-optionBasket"> <ShoppingBasketIcon /> <span className="header-optionLineTwo header-basketCount"> {basket?.length} </span> </div> </Link> </div> </div> ); }
             <span className="header-optionLineOne">
               Hello {!user ? "Guest" : user.email}
             </span>
@@ -65,4 +71,5 @@ function Header() {
   );
 }
 
+export { Itr_user_email };
 export default Header;
