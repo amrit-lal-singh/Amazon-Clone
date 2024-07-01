76bad5196f3eac86a165acbf06284ca8a5577282
Iterate-AI <support@iterate.ai.com>
2024-07-01 20:01:35 +0530

    Apply diff to multiple files

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -1,8 +1,44 @@
+import mixpanel from 'mixpanel-browser';
 import React from "react";
 import { useStateValue } from "../../StateProvider";
 import "./Product.css";
 
 function Product({ id, title, image, price, rating }) {
+    const [{ basket }, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        //dispatch the item into the data layer
+        dispatch({ type: "ADD_TO_BASKET", item: { id: id, title: title, image: image, price: price, rating: rating, }, });
+        // Mixpanel tracking
+        mixpanel.track('Add to basket', {
+            'product-price': price,
+            'add_to_basket_pressed': true
+        });
+    };
+
+    return (
+        <div className="product">
+            <div className="product-info">
+                <p>{title}</p>
+                <p className="product-price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product-rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p>⭐</p>
+                        ))}
+                </div>
+            </div>
+            <img src={image} alt="" />
+            <button onClick={addToBasket}> Add to basket</button>
+        </div>
+    );
+}
+
+export default Product;
   const [{ basket }, dispatch] = useStateValue();
 
   const addToBasket = () => {
