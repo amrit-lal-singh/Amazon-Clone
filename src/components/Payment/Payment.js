31127e4aa6fc24092f619230ad55aa182581481f
Iterate-AI <support@iterate.ai.com>
2024-07-13 16:09:41 +0530

    Apply diff to multiple files

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import mixpanel from 'mixpanel-browser';
 import { useStateValue } from "../../StateProvider";
 import CheckoutProduct from "../Checkout/CheckoutProduct";
 import "./Payment.css";
@@ -44,8 +45,9 @@ function Payment() {
     event.preventDefault();
     setProcessing(true);
 
-    const payload = await stripe
-      .confirmCardPayment(clientSecret, {
+    // Mixpanel tracking
+    mixpanel.track('Buy Now', { number_items: basket?.length, user_email: user?.email });
+    const payload = await stripe.confirmCardPayment(clientSecret, {
         payment_method: {
           card: elements.getElement(CardElement),
         },
