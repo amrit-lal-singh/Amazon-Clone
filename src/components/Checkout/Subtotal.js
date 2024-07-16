6cfd60406bd145583377b971353524cad1707a47
Iterate-AI <support@iterate.ai.com>
2024-07-16 20:08:37 +0530

    Apply diff to multiple files

--- src/components/Checkout/Subtotal.js
+++ src/components/Checkout/Subtotal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import mixpanel from 'mixpanel-browser';
 import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "../../StateProvider";
